480d9916fc6b3c4fd00e15c3be7d69439c3e2a1b
Mazeqi <2631194340@qq.com>
2020-03-24 23:59:41 +0800

    2020/3/24/01

---
+++ BalancedTree/BTreeCode/test.cpp
@@ -0,0 +1,102 @@
+//#include"../CodeHead/BTreeHead.h"
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main(){
+//	//srand((unsigned int)time(NULL));
+//	char input[] = "CNGAHEKQMFWLTZDPRXYS";
+//	char input2[] = "ABCDEFGIJKLMNPRSTUXZ";
+//	BTree root = NULL;
+//	BTree root2 = NULL;
+//	BTree root3 = NULL;
+//	BTree root4 = NULL;
+//	BTree root5 = NULL;
+//
+//
+//	//≤‚ ‘“ª
+//
+//	for (int i = 1; i <= 1000; i++){
+//		InsertBTree(root, i);
+//	}
+//	DeleteBTree(root, 0);
+//	for (int i = 250; i >= 1; i--){
+//		DeleteBTree(root, i);
+//	}
+//	for (int i = 10; i < 1000; i++){
+//		DeleteBTree(root, i);
+//	}
+//	DeleteBTree(root, 500);
+//
+//	//≤‚ ‘2
+//	InsertBTree(root2, 100000);
+//	for (int i = 1; i < 100000; i++){
+//		InsertBTree(root2, i);
+//	}
+//	DeleteBTree(root2, 0);
+//	for (int i = 50000; i >= 1; i--){
+//		DeleteBTree(root2, i);
+//	}
+//	for (int i = 50000; i < 100000; i++){
+//		DeleteBTree(root2, i);
+//	}
+//	DeleteBTree(root2, 1);
+//
+//	//≤‚ ‘3
+//	/*for (int i = 0; i <= 19; i++){
+//		InsertBTree(root3, input[i]);
+//	}*/
+//	//DeleteBTree(root3, 'H');
+//	//DeleteBTree(root3, 'T');
+//	//DeleteBTree(root3, 'R');
+//	//DeleteBTree(root3, 'E');
+//
+//	//≤‚ ‘4
+//	/*for (int i = 0; i <= 19; i++){
+//		InsertBTree(root4, input2[i]);
+//	}
+//	DeleteBTree(root4, 'C');
+//*/
+//	//≤‚ ‘5
+//	/*InsertBTree(root5, 100000);
+//	for (int i = 1; i < 100000; i++){
+//		InsertBTree(root5, i);
+//	}
+//	
+//	DeleteBTree(root5, 1);
+//	for (int i = 500; i >= 1; i--){
+//		DeleteBTree(root5, i);
+//	}
+//	for (int i = 1; i < 100000; i++){
+//		DeleteBTree(root5, i);
+//	}
+//	DeleteBTree(root5, 500);*/
+//
+//	
+//	
+//	
+//	
+//	
+//	/*DeleteBTree(root3, 'C');
+//	DeleteBTree(root3, 'N');
+//	DeleteBTree(root3, 'G');
+//	DeleteBTree(root3, 'A');
+//	DeleteBTree(root3, 'H');
+//	DeleteBTree(root3, 'E');
+//	DeleteBTree(root3, 'K');
+//	DeleteBTree(root3, 'Q');
+//	DeleteBTree(root3, 'M');
+//	DeleteBTree(root3, 'F');
+//	DeleteBTree(root3, 'W');
+//	DeleteBTree(root3, 'L');
+//	DeleteBTree(root3, 'T');
+//	DeleteBTree(root3, 'Z');
+//	DeleteBTree(root3, 'D');
+//	DeleteBTree(root3, 'P');
+//	DeleteBTree(root3, 'R');
+//	DeleteBTree(root3, 'X');
+//	DeleteBTree(root3, 'Y');
+//	DeleteBTree(root3, 'S');
+//*/
+//	system("pause");
+//	return 0;
+//}
